50ea543dc0736565c337ce2228d1ed1278e6413d
Isa Giongo <isagiongo@yahoo.com.br>
2018-10-08 21:04:52 -0300

    Inclusão for

--- js/principal.js
+++ js/principal.js
@@ -2,35 +2,42 @@ console.log("Arquivo externo");
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Teste";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+for(var i = 0; i < pacientes.length; i++){
+  var paciente = pacientes[i];
 
-var tdImc = document.querySelector(".info-imc");
+  var tdPeso = paciente.querySelector(".info-peso");
+  var tdAltura = paciente.querySelector(".info-altura");
 
-var pesoValido = true;
-var alturaValida = true;
+  var peso = tdPeso.textContent;
+  var altura = tdAltura.textContent;
 
-if(peso <= 0 || peso >= 500){
-  pesoValido = false;
-  tdPeso.textContent = "Peso inválido";
+  var tdImc = paciente.querySelector(".info-imc");
+
+  var pesoValido = true;
+  var alturaValida = true;
+
+  if(peso <= 0 || peso >= 500){
+    pesoValido = false;
+    tdPeso.textContent = "Peso inválido";
+  }
+
+  if(altura <= 0 || altura >= 3.00){
+    alturaValida = false;
+    tdAltura.textContent = "Altura inválida";
+  }
+
+  if(pesoValido && alturaValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  } else{
+    tdImc.textContent = "Altura e/ou Peso inválidos"
+  }
+
+  console.log(peso);
+  console.log(altura);
+  console.log(imc);
+
 }
-
-if(altura <= 0 || altura >= 3.00){
-  alturaValida = false;
-  tdAltura.textContent = "Altura inválida";
-}
-
-if(pesoValido && alturaValida){
-  var imc = peso / (altura * altura);
-  tdImc.textContent = imc;
-} else{
-  tdImc.textContent = "Altura e/ou Peso inválidos"
-}
-
-console.log(peso);
-console.log(altura);
-console.log(imc);
