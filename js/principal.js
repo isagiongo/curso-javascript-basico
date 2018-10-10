a910c0227590d7faa7d6217c060898259311619a
Isa Giongo <isagiongo@yahoo.com.br>
2018-10-09 22:18:07 -0300

    adicionar novo paciente atraves do form

--- js/principal.js
+++ js/principal.js
@@ -22,11 +22,13 @@ for(var i = 0; i < pacientes.length; i++){
   if(peso <= 0 || peso >= 500){
     pesoValido = false;
     tdPeso.textContent = "Peso inválido";
+    paciente.classList.add("paciente-invalido");
   }
 
   if(altura <= 0 || altura >= 3.00){
     alturaValida = false;
     tdAltura.textContent = "Altura inválida";
+    paciente.classList.add("paciente-invalido");
   }
 
   if(pesoValido && alturaValida){
@@ -41,3 +43,35 @@ for(var i = 0; i < pacientes.length; i++){
   console.log(imc);
 
 }
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+  event.preventDefault();
+  var form = document.querySelector("#form-adiciona");
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(pacienteTr);
+
+})
